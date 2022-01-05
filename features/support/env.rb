0b18ee108a55fc102b46b0c2a55a4ad0c3c9e135
Julio <julio958214@gmail.com>
2022-01-05 17:47:03 -0300

    refatoracao

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,15 +3,20 @@ require "capybara/cucumber"
 require "site_prism"
 require "rspec"
 require "rufo"
+require "selenium-webdriver"
 require "allure-cucumber"
+require "webdrivers"
 require_relative "../pages/restaurantes_page.rb"
 
 World(Actions)
 
-Capybara.configure do |config|
-  config.default_driver = :selenium_chrome_headless
-  config.app_host = "https://enjoeat-sp2.herokuapp.com"
-  config.default_max_wait_time = 30
+Capybara.configure do
+  include RSpec::Matchers
+  Capybara.run_server = false
+  Capybara.default_driver = :selenium_chrome_headless
+  Capybara.page.driver.browser.manage.window.maximize
+  Capybara.default_max_wait_time = 10
+  Capybara.app_host = "https://enjoeat-sp2.herokuapp.com"
 end
 
 AllureCucumber.configure do |config|
