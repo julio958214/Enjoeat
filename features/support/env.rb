448db22c3282b6ca590bccebe3a8e7469435db6c
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-01-27 11:16:36 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,7 +18,6 @@ Capybara.configure do
   include RSpec::Matchers
   Capybara.run_server = false
   Capybara.default_driver = :selenium_chrome_headless
-  Capybara.page.driver.browser.manage.window.maximize
   Capybara.default_max_wait_time = 10
   Capybara.app_host = "https://enjoeat-sp2.herokuapp.com"
 end
