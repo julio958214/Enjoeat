a9ac7d32206d9861c489d14ccf47d0dd28a65da3
Julio Santos <julio958214@gmail.com>
2022-05-01 22:57:25 -0300

    .

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,15 +10,45 @@ require_relative "../pages/restaurantes_page.rb"
 
 World(Actions)
 
-Capybara.register_driver :chorme do |app|
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-    "goog:chromeOptions" => {
-      "args" => ["--headless", "--disable-site-isolation-trials", "--disable-gpu"],
-      "excludeSwitches" => ["enable-logging"],
-    },
-  )
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :capabilities => caps)
+
+BROWSER = ENV.fetch('BROWSER')
+
+  Capybara.register_driver :chorme do |app|
+      if BROWSER.eql?('chrome')
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        "goog:chromeOptions" => {
+          "args" => ["--chrome", "--disable-site-isolation-trials", "--disable-gpu"],
+          "excludeSwitches" => ["enable-logging"],
+        },
+      )
+      Capybara::Selenium::Driver.new(app, :browser => :chrome, :capabilities => caps)
+
+    elsif BROWSER.eql?('headless')
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        "goog:chromeOptions" => {
+          "args" => ["--headless", "--disable-site-isolation-trials", "--disable-gpu"],
+          "excludeSwitches" => ["enable-logging"],
+        },
+      )
+      Capybara::Selenium::Driver.new(app, :browser => :chrome, :capabilities => caps)
+
+    elsif BROWSER.eql?('firefox')
+      Capybara::Selenium::Driver.new(app, browser: :firefox)
+  
+    elsif BROWSER.eql?('firefox_headless')
+      headless = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
+      Capybara::Selenium::Driver.new(app, browser: :firefox, capabilities: headless)
+  
+    elsif BROWSER.eql?('edge')
+      edge = Selenium::WebDriver::Edge::Options.new(args: ['--edge'], excludeSwitches: ['enable-logging'])
+      Capybara::Selenium::Driver.new(app, browser: :edge, capabilities: edge)
+  
+    elsif BROWSER.eql?('edge_headless')
+      edge = Selenium::WebDriver::Edge::Options.new(args: ['--headless'], excludeSwitches: ['enable-logging'])
+      Capybara::Selenium::Driver.new(app, browser: :edge, capabilities: edge)
+  end
 end
+
 
 Capybara.configure do
   include RSpec::Matchers
@@ -32,3 +62,4 @@ AllureCucumber.configure do |config|
   config.results_directory = "/logs"
   config.clean_results_directory = true
 end
+
